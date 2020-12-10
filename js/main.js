b6ef550696848e5798a4a30abe803e39ebdc661b
drnewgate <alekseibuhtin@mail.ru>
2020-12-10 22:42:09 +0300

    fix: поменял карту яндекса на гугл

--- js/main.js
+++ js/main.js
@@ -11,24 +11,3 @@ var mySwiper = new Swiper('.swiper-container', {
     enabled: true,
   },
 })
-// Yandex.Map
-var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
-
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center:[7.83812217, 98.29887402], //Пхукет        
-        zoom:15
-    });
-
-    document.getElementById('destroyButton').onclick = function () {
-        // Для уничтожения используется метод destroy.
-        myMap.destroy();
-    };
-}
