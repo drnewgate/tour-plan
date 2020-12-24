248179eb0fdb32aa9fbd9dfee5603bc42fcbefba
drnewgate <alekseibuhtin@mail.ru>
2020-12-24 23:03:34 +0300

    create php model for email

--- send.php
+++ send.php
@@ -8,14 +8,16 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 // Формирование самого письма
-$title = "Заголовок письма";
+$title = "Новое обращение Best Tour Plan";
 $body = "
-<h2>Новое письмо</h2>
+<h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
-<b>Почта:</b> $email<br><br>
-<b>Сообщение:</b><br>$text
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+<b>Почта:</b><br>$email
 ";
 
 // Настройки PHPMailer
@@ -28,34 +30,20 @@ try {
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'your_login'; // Логин на почте
-    $mail->Password   = 'password'; // Пароль на почте
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'alekseibuhtin@mail.ru'; // Логин на почте
+    $mail->Password   = 'prometeus16499'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('mail@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('alekseibuhtin@mail.ru', 'Алексей Бухтин'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('youremail@yandex.ru');  
-    $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+    $mail->addAddress('drnewgate@yandex.ru');  
 
-    // Прикрипление файлов к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
-        }
-    }   
-}
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
@@ -67,4 +55,4 @@ else {$result = "error";}
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+header('Location: thankyou.html');
