0ab36e29fbf2324f5fb736c1fc5ae7746ce35a7c
drnewgate <alekseibuhtin@mail.ru>
2020-12-25 23:39:31 +0300

    fix differen mistakes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -11,7 +12,7 @@ var hotelSlider = new Swiper('.hotel-slider', {
     enabled: true,
   },
 })
-var reviewsSlider = new Swiper('.reviews-slider', {
+  var reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
   
@@ -23,14 +24,25 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   keyboard: {
     enabled: true,
   },
-})
+  })
 
-const menuButton = $('.menu-button')
-  menuButton.on('click', function () {
-    const navbarBottom = $('.navbar-bottom')
-    navbarBottom.toggleClass('navbar-bottom--visible')
-    
-    navbarBottom.hasClass('navbar-bottom--visible')
-      ? document.body.style.overflow = 'hidden'
-      : document.body.style.overflow = ''
-});
+    const menuButton = $('.menu-button')
+      menuButton.on('click', function () {
+        const navbarBottom = $('.navbar-bottom')
+        navbarBottom.toggleClass('navbar-bottom--visible')
+        
+        navbarBottom.hasClass('navbar-bottom--visible')
+          ? document.body.style.overflow = 'hidden'
+          : document.body.style.overflow = ''
+      });
+  var modalButton = $('[data-toggle=modal]');
+      modalButton.on('click', openModal);
+
+      function openModal() {
+        var modalOverlay = $('.modal__overlay');
+        var modalDialog = $('.modal__dialog');
+        modalOverlay.addClass('modal__overlay--visible')
+        modalDialog.addClass('modal__dialog--visible')
+
+      }
+  });
