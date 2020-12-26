ddc7d585761e100e7d07119e0e8cfe977c3932c5
drnewgate <alekseibuhtin@mail.ru>
2020-12-26 13:20:40 +0300

    create js for modal

--- js/main.js
+++ js/main.js
@@ -36,13 +36,21 @@ var hotelSlider = new Swiper('.hotel-slider', {
           : document.body.style.overflow = ''
       });
   var modalButton = $('[data-toggle=modal]');
-      modalButton.on('click', openModal);
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
 
       function openModal() {
+        var targetModal = $(this).attr('data-href');
+        $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+        $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+      }
+      function closeModal(event) {
+        event.preventDefault();
         var modalOverlay = $('.modal__overlay');
         var modalDialog = $('.modal__dialog');
-        modalOverlay.addClass('modal__overlay--visible')
-        modalDialog.addClass('modal__dialog--visible')
+        modalOverlay.removeClass('modal__overlay--visible')
+        modalDialog.removeClass('modal__dialog--visible')
 
       }
   });
